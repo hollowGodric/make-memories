1b47cf75d441687927e536e74bbc8b0ec4dfee89
brian <brian.kroll@semigator.de>
2018-01-29 18:23:16 +0100

    trying to refactor

--- src/AppBundle/Entity/DiscordUser.php
+++ src/AppBundle/Entity/DiscordUser.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * DiscordUser
@@ -10,7 +11,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="discord_user")
  * @ORM\Entity(repositoryClass="AppBundle\Repository\DiscordUserRepository")
  */
-class DiscordUser
+class DiscordUser implements UserInterface
 {
     const AVATAR_PATTERN = 'https://cdn.discordapp.com/avatars/%u/%s.png';
 
@@ -185,6 +186,63 @@ class DiscordUser
     public function setToken(AccessToken $token)
     {
         $this->token = $token;
+    }
+
+    /**
+     * Returns the roles granted to the user.
+     *
+     * <code>
+     * public function getRoles()
+     * {
+     *     return array('ROLE_USER');
+     * }
+     * </code>
+     *
+     * Alternatively, the roles might be stored on a ``roles`` property,
+     * and populated in any number of different ways when the user object
+     * is created.
+     *
+     * @return (Role|string)[] The user roles
+     */
+    public function getRoles()
+    {
+        return ['ROLE_USER'];
+    }
+
+    /**
+     * Returns the password used to authenticate the user.
+     *
+     * This should be the encoded password. On authentication, a plain-text
+     * password will be salted, encoded, and then compared to this value.
+     *
+     * @return string The password
+     */
+    public function getPassword()
+    {
+        return '';
+    }
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+    {
+        return '';
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
+
     }
 }
 
